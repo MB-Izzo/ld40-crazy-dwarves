a239173654d83dd40ee940948c7588545c628eef
Mathieu Bouchonnet <mathieubouchonnet@gmail.com>
2017-12-02 12:54:36 +0100

    FSM Working

---
+++ Assets/Scripts/MYFSM/FSMState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FSMState<T> {
+
+	abstract public void Enter(T entity);
+	abstract public void Execute(T entity);
+	abstract public void Exit(T entity);
+
+}
