37aa9eb6c6eb6e7ebad3cd0de6fff7b513ae47ed
Laurent Victorino <lvictorino@monkeymoon.net>
2017-12-02 18:06:39 +0100

    State Machine improvement.

--- Assets/Scripts/FSM/Dwarf.cs
+++ Assets/Scripts/FSM/Dwarf.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dwarf : MonoBehaviour {
+// NOTNEEDED: DwarfBehavior does all the job.
 
-	private Vector3 brewery_pos;
-	private int _thirst = 100;
-	public int thirst { get { return _thirst; } }
+//public class Dwarf : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	}
+//	private Vector3 brewery_pos;
+//	private int _thirst = 100;
+//	public int thirst { get { return _thirst; } }
+
+//	// Use this for initialization
+//	void Start () {
+//	}
 	
-	// Update is called once per frame
-	void Update () {
+//	// Update is called once per frame
+//	void Update () {
 
-	}
-}
+//	}
+//}
--- Assets/Scripts/FSM/StateMachine.cs
+++ Assets/Scripts/FSM/StateMachine.cs
@@ -2,36 +2,79 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateMachine : MonoBehaviour {
-	
-	private IState _current_state;
-	private IState _previous_text;
-
-	public void ChangeState(IState new_state)
-	{
-		if (this._current_state != null)
-		{
-			this._current_state.Exit();
-		}
-
-		this._previous_text = this._current_state;
-		this._current_state = new_state;
-		this._current_state.Enter();
-	}
-
-	public void ExecuteStateUpdate()
-	{
-		IState running_state = this._current_state;
-		if (running_state != null)
-		{
-			running_state.Execute();
-		}
-	}
-
-	public void SwitchToPreviousState()
-	{
-		this._current_state.Exit();
-		this._current_state = this._previous_text;
-		this._current_state.Enter();
-	}
+public enum eStateType
+{
+    none = 0,
+    moving,
+    using_building,
 }
+
+public class StateMachine
+{
+    private IState _current_state;
+
+    public void Update()
+    {
+        if (_current_state != null)
+        {
+            IState new_state = _current_state.Execute();
+            if ( new_state != null )
+            {
+                OverrideCurrentState(new_state);
+            }
+        }
+    }
+
+    public void OverrideCurrentState(IState new_state)
+    {
+        if ( _current_state != null )
+        {
+            _current_state.Exit();
+        }
+        _current_state = new_state;
+        if (_current_state != null)
+        {
+            _current_state.Enter();
+        }
+    }
+
+    public eStateType GetCurrentStateType()
+    {
+        return _current_state == null ? eStateType.none : _current_state.GetStateType();
+    }
+}
+
+// NOTNEEDED: new implementation above.
+//public class StateMachine : MonoBehaviour {
+	
+//	private IState _current_state;
+//	private IState _previous_text;
+
+//	public void ChangeState(IState new_state)
+//	{
+//		if (this._current_state != null)
+//		{
+//			this._current_state.Exit();
+//		}
+
+//		this._previous_text = this._current_state;
+//		this._current_state = new_state;
+//		this._current_state.Enter();
+//	}
+
+//	public void ExecuteStateUpdate()
+//	{
+//		IState running_state = this._current_state;
+//		if (running_state != null)
+//		{
+//			running_state.Execute();
+//		}
+//	}
+
+//	public void SwitchToPreviousState()
+//	{
+//		this._current_state.Exit();
+//		this._current_state = this._previous_text;
+//		this._current_state.Enter();
+//	}
+//}
